96dc195205f9f0e72763d2c1a7aec9e838c4e436
sai ram <sairam2076@gmail.com>
2020-04-23 02:36:10 +0530

    promotion service updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishService } from './services/dish.service';
-
+import { PromotionService} from './services/promotion.service';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -46,7 +46,10 @@ import { ContactComponent } from './contact/contact.component';
     MatButtonModule,
     MatListModule
   ],
-  providers: [DishService],
+  providers: [
+    DishService,
+    PromotionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
