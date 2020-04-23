ad2c37ee88e8238d223c74921327231a0bc92cc3
sai ram <sairam2076@gmail.com>
2020-04-23 13:16:00 +0530

    routing parameter added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ export const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'menu',     component: MenuComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'dishdetail/:id', component: DishdetailComponent },
   { path: 'contactus',     component: ContactComponent },
 ];
 
